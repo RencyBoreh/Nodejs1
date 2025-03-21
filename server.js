b45f77105b5110b982539cd5aa6ad52d44115aca
RencyBoreh <jeptanuirency313@gmail.com>
2025-03-21 11:03:51 +0300

    change

--- server.js
+++ server.js
@@ -10,23 +10,36 @@
 //   console.log(`Server Running on Port:${PORT}`);
   
 // })
-
+import fs from 'fs/promises';
+import { error } from 'console';
 import http from 'http';
 const port=8000;
 // const name="Jerop"
 const server=http.createServer((req,res)=>{
-  if (req.url==='/') {
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.end(`<h1 style='text-align:center;'>Home Page.</h1>`);
+  try {
+    //checking for GET method
+    if (req.method==='GET') {
+      if (req.url==='/') {
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end(`<h1 style='text-align:center;'>Home Page.</h1>`);
+      }
+      else if (req.url==='/about') {
+        res.writeHead(200,{'Content-Type':'text/html'});
+      res.end(`<h1 style='text-align:center;'>Hello this is About Page.</h1>`);
+      }
+      else{
+        res.writeHead(404,{'Content-Type':'text/html'});
+        res.end(`<h1 style='text-align:center;'>Page Not Found.</h1>`);
+      }
+    }
+    else{
+      throw new error('Method Not Allowed!!')
+    }
+  } catch (error) {
+    res.writeHead(500,{'Content-Type':'text/html'});
+    res.end(`<h1 style='text-align:center;'>Server Error.</h1>`);
   }
-  else if (req.url==='/about') {
-    res.writeHead(200,{'Content-Type':'text/html'});
-  res.end(`<h1 style='text-align:center;'>Hello this is About Page.</h1>`);
-  }
-  else{
-    res.writeHead(404,{'Content-Type':'text/html'});
-    res.end(`<h1 style='text-align:center;'>Page Not Found.</h1>`);
-  }
+ 
  
  
   
